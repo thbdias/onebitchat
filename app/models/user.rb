2b673fb47ea4cb6b3f1282e7d333465bcc1e833f
thbdias <thiagobalbino91@gmail.com>
2019-03-25 23:32:57 -0300

    criando os nossos models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
+  
+  has_many :teams
+  has_many :messages
+  has_many :talks
 end
