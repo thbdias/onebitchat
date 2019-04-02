045ec3fb639aeeb231ecb498aad679161147641f
thbdias <thiagobalbino91@gmail.com>
2019-04-01 22:16:06 -0300

    factories

---
+++ spec/factories/message.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :message do
+    body    { FFaker::Lorem.sentence }
+    user    # cria um user random
+  end
+ end
---
+++ spec/factories/team_user.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :team_user do
+    team  # cria um team random
+    user  # cria um user random
+  end
+end
